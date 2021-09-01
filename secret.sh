354130666a47099b0f7a4de9b9944c19788f5d9e
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-09-01 18:01:29 -0300

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -44,8 +44,9 @@ uninstall_1(){
   case "$resposta_1" in
     Y|y)  echo -e "Desinstalando..." ;
       sleep 3 ;
-      rm -f $0 && rm -rf ~/secret ;
-      cd ;;
+      rm -f $0 && rm -rf ~/$0 ;
+      cd .. ;
+      exit 0 ;;
     N|n)  exit 0 ;;
     '\n') exit 0 ;; #[[ $enter_return ]] && return "$enter_return"
   esac
